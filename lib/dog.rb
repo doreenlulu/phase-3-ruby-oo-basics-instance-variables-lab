9f29ffc0ab31e685069edeab6c902d0b8a46e7cb
Doreen <doreenlulu97@gmail.com>
2022-10-22 00:22:31 +0300

    Done with asignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,10 @@
 class Dog
     
-def name=(dog_name)
-    @this_dogs_name = dog_name
-end
-
-def name
-    @this_dogs_name
+  def name=(dog_name)
+      @this_dogs_name = dog_name
   end
-end
 
-name_dog = Dog.new
-name_dog.name = "rose"
-puts name_dog.name
+  def name
+      @this_dogs_name
+    end
+end
